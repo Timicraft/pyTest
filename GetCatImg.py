e67fafc1e11b2c304450c54003c1f33b29e07c60
Timicraft <446230186@qq.com>
2018-09-16 19:48:04 +0800

    获取猫咪图片的爬虫
    从placekitten上获取随机图片大小的猫咪图片，试图使用代理IP，但是失败了

---
+++ GetCatImg.py
@@ -0,0 +1,30 @@
+import urllib.request as url
+import random
+
+
+n = input("输入要下载的猫咪图片的数量：")
+n = int(n)
+
+#IP = ['210.72.14.142:80','112.12.37.196:53281','180.110.6.221:3128']
+
+for x in range(n):
+
+    #proxy_support = url.ProxyHandler({'http':random.choice(IP)})
+
+    #opener = url.build_opener(proxy_support)
+
+    #url.install_opener(opener)
+    
+    size_x = 500+int(random.randint(1,20))
+    size_y = 500+int(random.randint(1,20))
+    
+    name_url = 'http://placekitten.com/g/'+str(size_x)+'/'+str(size_y)
+    response = url.urlopen(name_url)
+    
+    img = response.read()
+    name = 'cat_'+str(size_x)+'_'+str(size_y)+'_'+str(x)+'.jpg'
+    
+    with open(name,'wb') as f:
+        f.write(img)
+
+    
