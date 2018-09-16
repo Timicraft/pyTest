b8e1a76df96411b08202894b49a34adc2a86559c
Timicraft <446230186@qq.com>
2018-09-16 18:02:18 +0800

    Initial commit

---
+++ CountList.py
@@ -0,0 +1,18 @@
+class CountList:
+
+    def __init__(self,*args):
+
+        self.value=[x for x in args]
+
+        self.count={}.fromkeys(range(len(self.value)),0)
+
+    def __len__(self,value):
+
+        return len(self.value)
+
+    def __getitem__(self,key):
+
+        self.count[key] += 1
+
+        return self.value[key]
+
